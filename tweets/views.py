1472b0b95569e99b6d7ae0cb9dcad6cf71a07962
Stanislav <swatogor@yandex.ru>
2021-02-23 13:56:21 +0300

    Created tweet detail view

--- tweets/views.py
+++ tweets/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
 
 from .models import Tweet
@@ -8,5 +8,17 @@ def home_view(request, *args, **kwargs):
     return HttpResponse('<h1>Hello world!</h1>')
 
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
-    obj = Tweet.objects.get(id=tweet_id)
-    return HttpResponse(f"<h1>Hello {tweet_id}</h1>")   
+    data ={
+        "id": tweet_id
+    }
+    
+    status = 200
+
+    try:
+        obj = Tweet.objects.get(id=tweet_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = 'Not found'
+        status = 404
+    
+    return JsonResponse(data, status=status)
